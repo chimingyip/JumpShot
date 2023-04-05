cfb7ddbc5e51b0e42c15a9185013fb03b3e93b7e
Chi-Ming Yip <cmyip99@gmail.com>
2023-04-06 00:25:20 +1000

    Trajectory line added

--- Assets/Scripts/DragAndJump.cs
+++ Assets/Scripts/DragAndJump.cs
@@ -14,10 +14,12 @@ public class DragAndJump : MonoBehaviour
     private Vector2 force;
     private Vector3 startPoint;
     private Vector3 endPoint;
+    private LineTrajectory tl;
 
     private void Start()
     {
         cam = Camera.main;
+        tl = GetComponent<LineTrajectory>();
     }
 
     private void Update()
@@ -28,14 +30,21 @@ public class DragAndJump : MonoBehaviour
             startPoint.z = 15;
         }
 
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            currentPoint.z = 15;
+            tl.RenderLine(startPoint, currentPoint);
+        }
+
         if (Input.GetMouseButtonUp(0))
         {
             endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
             endPoint.z = 15;
 
             force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
-
             rb.AddForce(force * power, ForceMode2D.Impulse);
+            tl.EndLine();
         }
     }
 }
--- Assets/Scripts/LineTrajectory.cs
+++ Assets/Scripts/LineTrajectory.cs
@@ -12,8 +12,18 @@ public class LineTrajectory : MonoBehaviour
         lr = GetComponent<LineRenderer>();
     }
 
-    public void RenderLine()
+    public void RenderLine(Vector3 startPoint, Vector3 endPoint)
     {
+        lr.positionCount = 2;
+        Vector3[] points = new Vector3[2];
+        points[0] = startPoint;
+        points[1] = endPoint;
 
+        lr.SetPositions(points);
+    }
+
+    public void EndLine()
+    {
+        lr.positionCount = 0;
     }
 }
