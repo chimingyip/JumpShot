b064e8442302461f8933b902a99dc56f100ee04a
Chi-Ming Yip <cmyip99@gmail.com>
2023-04-22 14:52:03 +1000

    Add damage for enemies

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour
 {
+    [SerializeField] private int damage;
+
     private void OnTriggerEnter2D(Collider2D collision) {
+
+        Damage damageableGameObject = collision.GetComponent<Damage>();
+
+        if (damageableGameObject) {
+            damageableGameObject.TakeDamage(damage);
+        }
+
         Destroy(gameObject);
     }
 }
